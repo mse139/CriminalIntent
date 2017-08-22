34db4cdd33520fe6c6bb7861ac287dabe218b291
mse139 <mse139@gmail.com>
2017-08-21 21:12:36 -0400

    chapter 8 complete

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by mike on 8/21/17.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
